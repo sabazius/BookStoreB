77aa3e4d8e942ea208e03e9d247508375baabda9
Boris Serafimov <b.serafimov@draftkings.com>
2023-03-09 17:28:39 +0200

    Book model

---
+++ BookStoreB/BookStore.Models/Data/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore.Models.Data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public string Description { get; set; }
+    }
+}
